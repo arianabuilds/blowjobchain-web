1c06e85b21d733721ee639b40bd25f6d53644e00
Ariana Ivan <git@arianaivan.com>
2024-05-02 01:13:00 -0700

    Store user's Name in db

--- src/app/SetYourName.tsx
+++ src/app/SetYourName.tsx
@@ -1,10 +1,15 @@
+"use client"
+
+import { createClient } from "@/supabase/client"
+
 export const SetYourName = () => {
   return (
-    <div className="flex flex-col justify-between">
+    <form action={SetYourNameAction} className="flex flex-col justify-between">
       <div className="text-center">
         <h3 className="text-xl">Set your name:</h3>
         <input
           type="text"
+          name="name"
           className="rounded-xl p-3 py-4 text-[21px] max-w-[250px] my-3 text-center"
           placeholder="Name"
         />
@@ -13,9 +18,23 @@ export const SetYourName = () => {
           <p>Can change anytime</p>
         </div>
       </div>
-      <button className="border border-blue-800/60 text-blue-900 rounded px-2 py-3 hover:bg-blue-700/20">
+      <button
+        className="border border-blue-800/60 text-blue-900 rounded px-2 py-3 hover:bg-blue-700/20"
+        type="submit"
+      >
         Next →
       </button>
-    </div>
+    </form>
   )
+}
+
+async function SetYourNameAction(formData: FormData) {
+  const name = formData.get("name")
+  // console.log("running SetYourName", name)
+
+  const supabase = createClient()
+  const user_id = (await supabase.auth.getUser()).data.user?.id
+
+  const { data, error } = await supabase.from("profiles").insert({ name, user_id })
+  // console.log("supa response", data, error)
 }
