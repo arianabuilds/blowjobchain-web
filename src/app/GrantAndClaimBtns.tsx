07c9ab520ba113ab8e535d75974e408c37c702aa
Ariana Ivan <git@arianaivan.com>
2024-05-03 20:41:35 -0700

    Add points / cards explanation above buttons

--- src/app/GrantAndClaimBtns.tsx
+++ src/app/GrantAndClaimBtns.tsx
@@ -2,9 +2,12 @@ const buttonClasses = `px-10 py-2 border-2 rounded-md text-gray-800 transition f
 
 export const GrantAndClaimBtns = () => {
   return (
-    <div className="flex justify-center space-x-10">
-      <button className={`${buttonClasses} border-blue-400/70 bg-blue-300`}>Grant</button>
-      <button className={`${buttonClasses} border-purple-400/80 bg-purple-300/80`}>Claim</button>
+    <div>
+      <p className="mb-3 text-center italic opacity-40 text-lg">10 points = 1 blowjob card</p>
+      <div className="flex justify-center space-x-10">
+        <button className={`${buttonClasses} border-blue-400/70 bg-blue-300`}>Grant</button>
+        <button className={`${buttonClasses} border-purple-400/80 bg-purple-300/80`}>Claim</button>
+      </div>
     </div>
   )
 }
