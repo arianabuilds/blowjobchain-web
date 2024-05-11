574eeb82e88a4e4456b0abc3e41d3f9621cf868a
Ariana Ivan <git@arianaivan.com>
2024-05-10 22:35:47 -0700

    Begin loading points

--- src/app/MainScreen.tsx
+++ src/app/MainScreen.tsx
@@ -1,3 +1,4 @@
+import { createSupabaseServer } from "@/supabase/server"
 import { HelloName } from "./HelloName"
 import { InvitePartnerLink } from "./InvitePartnerLink"
 import { loadPartnerships } from "./load-partnerships"
@@ -6,17 +7,42 @@ export const MainScreen = async ({ name }: { name: string }) => {
   const { partnerships } = await loadPartnerships()
   const hasPartner = !!partnerships?.length
 
+  const records = await loadPoints()
+
   return (
     <div className="text-center">
       <HelloName name={name} />
 
-      {/* No records */}
-      <p className="border rounded-lg border-black/50 p-2 px-10 my-16 text-black/80">
-        No records yet
-      </p>
+      {!records?.length ? (
+        // No records
+        <p className="border rounded-lg border-black/50 p-2 px-10 my-16 text-black/80">
+          No records yet
+        </p>
+      ) : (
+        // List of points
+        <div className="mt-6 text-left">
+          {records.map((r, index) => (
+            <p
+              key={index}
+              className="border rounded-lg border-black/50 p-2 px-3 my-2 text-black/80"
+            >
+              <span className="mr-4">{r.created_at}</span> {r.to} +{r.amount} point
+              {r.amount !== 1 ? "s" : ""}
+            </p>
+          ))}
+        </div>
+      )}
 
       {/* Share invite link */}
       {!hasPartner && <InvitePartnerLink />}
     </div>
   )
+}
+
+async function loadPoints() {
+  const supabase = createSupabaseServer()
+  const { data: points, error } = await supabase.from("points").select()
+  if (error) return alert(`Error loading points: ${JSON.stringify(error)}`)
+
+  return points
 }
