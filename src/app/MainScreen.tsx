17f0f6f1b8bdced1b4a6641a5daac9bb1808af9b
Ariana Ivan <git@arianaivan.com>
2024-05-10 23:01:32 -0700

    List: Show 'To' name, instead of uuid

--- src/app/MainScreen.tsx
+++ src/app/MainScreen.tsx
@@ -8,13 +8,18 @@ export const MainScreen = async ({ name }: { name: string }) => {
   const { partnerships } = await loadPartnerships()
   const hasPartner = !!partnerships?.length
 
-  const records = await loadPoints()
+  const userIdToName = (partnerships || []).reduce(
+    (memo, p) => ({ ...memo, [p.inviter]: p.inviter_name, [p.invitee]: p.invitee_name }),
+    {} as Record<string, string | undefined>,
+  )
+
+  const points = await loadPoints()
 
   return (
     <div className="text-center">
       <HelloName name={name} />
 
-      {!records?.length ? (
+      {!points?.length ? (
         // No records
         <p className="border rounded-lg border-black/50 p-2 px-10 my-16 text-black/80">
           No records yet
@@ -22,15 +27,18 @@ export const MainScreen = async ({ name }: { name: string }) => {
       ) : (
         // List of points
         <div className="mt-6 text-left">
-          {records.map((r, index) => (
+          {points.map((r, index) => (
             <p
               key={index}
               className="border rounded-lg border-black/50 p-2 px-3 my-2 text-black/80"
             >
               <span className="mr-4">
-                {format(r.created_at).replace(" hours", "h").replace(" minutes", "m")}
+                {format(r.created_at)
+                  .replace(" hour", "h")
+                  .replace(" minute", "m")
+                  .replace("s ", " ")}
               </span>{" "}
-              {r.to} +{r.amount} point
+              {userIdToName[r.to]} +{r.amount} point
               {r.amount !== 1 ? "s" : ""}
             </p>
           ))}
