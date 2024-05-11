b09f6ea07abe2964758a89f46037b1fde8e2fde3
Ariana Ivan <git@arianaivan.com>
2024-05-11 01:12:07 -0700

    Calc balances from db data

--- src/app/Balance.tsx
+++ src/app/Balance.tsx
@@ -1,5 +1,6 @@
 "use server"
 
+import { createSupabaseServer } from "@/supabase/server"
 import { PartnershipWithName } from "./load-partnerships"
 
 export const Balance = async ({
@@ -9,13 +10,22 @@ export const Balance = async ({
   name: string
   partnerships: PartnershipWithName
 }) => {
-  const first = partnerships[0]
+  const firstPartnership = partnerships[0]
+
+  const balances = await getBalances(firstPartnership)
+  if (!balances) return <p>Error loading balances</p>
 
   // Partner's name is the one that's not ours
-  let partner = first.inviter_name
-  if (partner === name) partner = first.invitee_name
-
-  const { me, partner: partner_balance } = await getBalances()
+  let partner = firstPartnership.inviter_name
+  let partner_balance = balances[firstPartnership.inviter]
+  let my_balance = balances[firstPartnership.invitee]
+  // We assumed partner was inviter,
+  // if that was wrong, flip inviter and invitee
+  if (partner === name) {
+    partner = firstPartnership.invitee_name
+    partner_balance = balances[firstPartnership.invitee]
+    my_balance = balances[firstPartnership.inviter]
+  }
 
   return (
     <div className="text-center">
@@ -24,29 +34,27 @@ export const Balance = async ({
           {partner}: {partner_balance.toFixed(1)}
         </div>
         <div className="w-[7.5rem]">
-          {name}: {me.toFixed(1)}
+          {name}: {my_balance.toFixed(1)}
         </div>
       </div>
     </div>
   )
 }
 
-async function getBalances() {
-  // Download my most recent balance summary entry
-  const summary = { me: 0, partner: 0 }
+async function getBalances({ inviter, invitee }: { inviter: string; invitee: string }) {
+  // TODO: Download my most recent balance summary entry
+  const summary = { [inviter]: 0, [invitee]: 0 }
 
-  // Download all entries newer then balance summary
-  const newEntries = [
-    { from: "me", to: "partner", amount: 4 },
-    { from: "partner", to: "me", amount: 8 },
-    { from: "partner", to: "me", amount: 4 },
-  ]
+  // Download all entries [TODO: newer then balance summary]
+  const { data: newEntries, error } = await createSupabaseServer().from("points").select()
+  if (!newEntries)
+    return console.error(`Error loading new balance entries: ${JSON.stringify(error)}`)
 
   // Calc new balance
   const new_balance = { ...summary }
   newEntries.forEach((entry) => {
-    if (entry.to === "me") new_balance.me += entry.amount / 10
-    if (entry.to === "partner") new_balance.partner += entry.amount / 10
+    if (entry.to === inviter) new_balance[inviter] += entry.amount / 10
+    if (entry.to === invitee) new_balance[invitee] += entry.amount / 10
   })
 
   // TODO: If balance changed, store update
