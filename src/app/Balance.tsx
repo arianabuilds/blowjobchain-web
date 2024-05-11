70f445ea7340ca22bf8a872b055a8be0af6dd750
Ariana Ivan <git@arianaivan.com>
2024-05-11 00:55:57 -0700

    Placeholder logic for calc'ing balances

--- src/app/Balance.tsx
+++ src/app/Balance.tsx
@@ -15,13 +15,41 @@ export const Balance = async ({
   let partner = first.inviter_name
   if (partner === name) partner = first.invitee_name
 
+  const { me, partner: partner_balance } = await getBalances()
+
   return (
     <div className="text-center">
       <div className="flex justify-center space-x-12 mb-2">
-        <div className="w-[7.5rem]">{partner}: 0</div>
-        <div className="w-[7.5rem]">{name}: 0</div>
+        <div className="w-[7.5rem]">
+          {partner}: {partner_balance.toFixed(1)}
+        </div>
+        <div className="w-[7.5rem]">
+          {name}: {me.toFixed(1)}
+        </div>
       </div>
-      {/* {JSON.stringify(partnerships)} */}
     </div>
   )
+}
+
+async function getBalances() {
+  // Download my most recent balance summary entry
+  const summary = { me: 0, partner: 0 }
+
+  // Download all entries newer then balance summary
+  const newEntries = [
+    { from: "me", to: "partner", amount: 4 },
+    { from: "partner", to: "me", amount: 8 },
+    { from: "partner", to: "me", amount: 4 },
+  ]
+
+  // Calc new balance
+  const new_balance = { ...summary }
+  newEntries.forEach((entry) => {
+    if (entry.to === "me") new_balance.me += entry.amount / 10
+    if (entry.to === "partner") new_balance.partner += entry.amount / 10
+  })
+
+  // TODO: If balance changed, store update
+
+  return new_balance
 }
