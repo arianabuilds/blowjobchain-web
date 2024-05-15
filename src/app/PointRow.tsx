03ac0eeff5659ccc0aca14ad465cd7a8791e7ff1
Ariana Ivan <git@arianaivan.com>
2024-05-15 02:19:55 -0700

    PointsRow: Fix horiz alignment

--- src/app/PointRow.tsx
+++ src/app/PointRow.tsx
@@ -12,9 +12,10 @@ export const PointRow = ({ point, who }: { point: Tables<"points">; who: string 
       className={`border rounded-lg border-black/50 p-2 px-3 my-2 text-black/80 w-full ${point.comment ? "hover:bg-black/5 cursor-pointer" : ""}`}
       onClick={() => point.comment && setOpen(!open)}
     >
-      <div>
-        {/* Timestamp */}
-        <span className="inline-block w-[5rem] opacity-70 text-sm">
+      {/* First row */}
+      <div className="justify-between flex items-center">
+        {/* Left: Timestamp */}
+        <span className="inline-block w-12 opacity-70 text-sm text-left">
           {format(point.created_at)
             .replace("ago", "")
             .replace("just ", "")
@@ -25,19 +26,25 @@ export const PointRow = ({ point, who }: { point: Tables<"points">; who: string 
             .replace(" month", "mo")
             .replace(" year", "y")
             .replace("s ", " ")}
-        </span>{" "}
-        {/* who */}
-        <span className="mr-1">{who?.[0]}</span>
-        {/* Amount */}
-        <>
-          {point.amount > 0 && "+"}
-          {point.amount} point
-          {point.amount !== 1 ? "s" : ""}
-        </>
-        {/* Comment icon */}
-        <span className="inline-block w-[5rem] text-right">{point.comment ? "💬" : ""}</span>
+        </span>
+
+        {/* Center section */}
+        <div>
+          {/* who */}
+          <span className="mr-1">{who?.[0]}</span>
+          {/* Amount */}
+          <>
+            {point.amount > 0 && "+"}
+            {point.amount} point
+            {point.amount !== 1 ? "s" : ""}
+          </>
+        </div>
+
+        {/* Right: Comment icon */}
+        <span className="inline-block w-12 text-right">{point.comment ? "💬" : ""}</span>
       </div>
 
+      {/* Hidden second row */}
       {open && (
         <div className="text-sm opacity-60 mt-1 text-center">
           {'"'}
