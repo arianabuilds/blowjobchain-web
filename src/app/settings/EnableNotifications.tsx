e21746c440a21e9b1e6763bed9d30b1aaebb1229
Ariana Ivan <git@arianaivan.com>
2024-05-17 15:56:01 -0700

    Improve look of notifications button

--- src/app/settings/EnableNotifications.tsx
+++ src/app/settings/EnableNotifications.tsx
@@ -1,16 +1,28 @@
 "use client"
 
+import { useState } from "react"
 import { registerPushNotifications } from "../registerPushNotifications"
 
 export const EnableNotifications = () => {
-  const status = false // TODO: real status
+  const [status, setStatus] = useState(false)
 
   return (
     <div
-      className="border-2 border-red-500 rounded px-3 p-2 mx-3 hover:bg-red-500/10 cursor-pointer"
-      onClick={registerPushNotifications}
+      className="bg-black/5 justify-between px-5 rounded-xl py-2 flex items-center space-x-2 cursor-pointer"
+      onClick={async () => {
+        if (!status) await registerPushNotifications()
+
+        setStatus(!status)
+      }}
     >
-      Push Notifications: {"" + status}
+      <span>Notifications</span>
+
+      {/* Toggle Switch */}
+      <div className={`relative w-14 h-7 rounded-full ${!status ? "bg-gray-300" : "bg-green-500"}`}>
+        <div
+          className={`absolute top-0.5 w-6 h-6 bg-white rounded-full shadow transition transform ${!status ? "translate-x-1" : "translate-x-7"}`}
+        ></div>
+      </div>
     </div>
   )
 }
