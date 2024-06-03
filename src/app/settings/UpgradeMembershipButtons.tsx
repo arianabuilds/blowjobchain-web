0a87b3f6c038e8f537ce4b866e2e42c7f3c2d5b3
Ariana Ivan <git@arianaivan.com>
2024-06-02 19:21:49 -0700

    UpgradeMembershipButtons hover

--- src/app/settings/UpgradeMembershipButtons.tsx
+++ src/app/settings/UpgradeMembershipButtons.tsx
@@ -5,10 +5,10 @@ export const UpgradeMembershipButtons = () => {
 
   return (
     <div className="space-x-5">
-      <button onClick={onClick} className="bg-white/40 px-3 rounded-lg">
+      <button onClick={onClick} className="bg-white/40 hover:bg-white/50 px-3 rounded-lg">
         Yearly: $10
       </button>
-      <button onClick={onClick} className="bg-white/30 px-3 rounded-lg">
+      <button onClick={onClick} className="bg-white/30 hover:bg-white/40 px-3 rounded-lg">
         Monthly: $1
       </button>
     </div>
