f2c12802908f407f32a1f3e65bab4261d97b429d
Ariana Ivan <git@arianaivan.com>
2024-06-06 03:38:20 -0700

    Settings: Tweak AddNewPartner hover/active

--- src/app/settings/partnership/AddNewPartner.tsx
+++ src/app/settings/partnership/AddNewPartner.tsx
@@ -8,7 +8,7 @@ export const AddNewPartner = () => {
   return (
     <div className="mb-1 text-center">
       <button
-        className="border border-purple-500/50 text-purple-800/50 text-sm rounded px-3 py-0.5 hover:bg-purple-800/10 active:bg-purple-800/20"
+        className="border border-purple-500/50 text-purple-800/50 text-sm rounded px-3 py-0.5 hover:bg-purple-500/10 active:bg-purple-600/20"
         onClick={() => setPressed(!pressed)}
       >
         + Add New Partner
