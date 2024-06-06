aa84ac0b6f94e4b5b4825d784e295ec7d6768445
Ariana Ivan <git@arianaivan.com>
2024-06-05 23:46:09 -0700

    DRY & active: UpgradeMembershipButtons

--- src/app/settings/UpgradeMembershipButtons.tsx
+++ src/app/settings/UpgradeMembershipButtons.tsx
@@ -1,16 +1,17 @@
 "use client"
 
 export const UpgradeMembershipButtons = () => {
-  const onClick = () => alert("coming soon")
-
   return (
     <div className="space-x-5 text-sm">
-      <button onClick={onClick} className="bg-white/40 hover:bg-white/50 px-3 rounded-lg">
-        Yearly: $10
-      </button>
-      <button onClick={onClick} className="bg-white/30 hover:bg-white/40 px-3 rounded-lg">
-        Monthly: $1
-      </button>
+      {[["Yearly: $10"], ["Monthly: $1"]].map(([label]) => (
+        <button
+          key={label}
+          onClick={() => alert("coming soon")}
+          className="bg-white bg-opacity-40 hover:bg-opacity-50 active:bg-opacity-70 px-3 rounded-lg"
+        >
+          {label}
+        </button>
+      ))}
     </div>
   )
 }
