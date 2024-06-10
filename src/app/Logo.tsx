d3506cfad53b1dc8a4d8671b34916386d5e06626
Ariana Ivan <git@arianaivan.com>
2024-06-10 14:08:24 -0700

    changed logo color to light zinc & font weight to medium

--- src/app/Logo.tsx
+++ src/app/Logo.tsx
@@ -1,3 +1,5 @@
 export const Logo = ({ big }: { big?: boolean }) => (
-  <p className={`${big ? "text-4xl pt-24" : "text-2xl pt-5"} font-semibold`}>BlowjobChain</p>
+  <p className={`${big ? "text-4xl pt-24" : "text-2xl pt-5"} font-medium text-zinc-400/80`}>
+    BlowjobChain
+  </p>
 )
