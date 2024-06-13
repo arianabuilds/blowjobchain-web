a286566158df70bc1d3a433dc1c63b3f5158cf62
Ariana Ivan <git@arianaivan.com>
2024-06-12 20:59:39 -0700

    Fix InvitePartnerLink icon positioning

--- src/app/logged-in/InvitePartnerLink.tsx
+++ src/app/logged-in/InvitePartnerLink.tsx
@@ -10,9 +10,7 @@ export const InvitePartnerLink = () => {
 
   return (
     <div className="flex">
-      <div className="text-[36px] pr-2">
-        <Image src={LinkIcon} alt="Link Icon" />
-      </div>
+      <Image src={LinkIcon} alt="Link Icon" className="text-[36px] pr-2" />
       <div className="flex flex-col justify-center overflow-hidden">
         {user_id === undefined ? (
           <p className="text-sm italic opacity-50">Loading your custom invite link...</p>
--- src/app/logged-in/PrePartner.tsx
+++ src/app/logged-in/PrePartner.tsx
@@ -4,7 +4,7 @@ import { NoRecordsYet } from "./NoRecordsYet"
 
 export const PrePartner = ({ name }: { name: string }) => {
   return (
-    <div className="text-center flex-1 mt-20">
+    <div className="text-center flex-1 mt-20 w-full px-4 max-w-[22rem] mx-auto">
       <HelloName name={name} />
 
       <NoRecordsYet />
