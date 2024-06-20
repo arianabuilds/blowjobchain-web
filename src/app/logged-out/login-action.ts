2acdf595bad174d9c0f42c6d56cacb4885bd01e6
Ariana Ivan <git@arianaivan.com>
2024-06-20 10:53:38 -0700

    Replace supabase's login email w/ our own

--- src/app/logged-out/login-action.ts
+++ src/app/logged-out/login-action.ts
@@ -1,26 +1,28 @@
 "use server"
 
 import { redirect } from "next/navigation"
+import { createTransport } from "nodemailer"
 
 import { createSupabaseServer } from "@/supabase/server"
 import { createSupabaseAdmin } from "@/supabase/admin"
 
 export async function login(formData: FormData) {
-  const supabase = createSupabaseServer()
-
   // type-casting for convenience, should validate
   const email = formData.get("email") as string
 
   if (!email) return
 
-  const { error } = await supabase.auth.signInWithOtp({ email })
+  const { data, error } = await createSupabaseAdmin().auth.admin.generateLink({
+    email,
+    type: "magiclink",
+  })
+  if (error) return console.log("log-in error:", error), redirect("/error")
+
+  await sendLoginEmail(email, data.properties)
 
   // If there was an inviterID, store the relationship
   const inviterID = formData.get("inviter-id")
   if (typeof inviterID === "string") storePartnershipInvitation(inviterID, email)
-
-  if (error) console.log("log-in error:", error)
-  // if (error) redirect("/error")
 
   redirect(
     `?enter-login-code&email=${email}${typeof inviterID === "string" ? `&u=${inviterID}` : ""}`,
@@ -58,4 +60,36 @@ async function storePartnershipInvitation(inviterID: string, inviteeEmail: strin
   if (!newUser?.[0]) return console.error("Error finding new invitee", inviterID, inviteeEmail)
 
   return supaAdmin.from("partnerships").insert({ inviter: inviterID, invitee: newUser[0].id })
+}
+
+const { SMTP_SERVER, SMTP_PORT, SMTP_USERNAME, SMTP_PASSWORD } = process.env
+const transporter = createTransport({
+  // @ts-expect-error
+  host: SMTP_SERVER,
+  port: SMTP_PORT,
+  secure: true,
+  auth: { user: SMTP_USERNAME, pass: SMTP_PASSWORD },
+})
+
+async function sendLoginEmail(
+  recipient_email: string,
+  {
+    action_link,
+    email_otp,
+  }: {
+    action_link: string
+    email_otp: string
+  },
+) {
+  try {
+    await transporter.sendMail({
+      from: '"BlowjobChain" <blowjobchain@gmail.com>',
+      to: recipient_email,
+      subject: `Confirm Your Email: ${email_otp}`,
+      html: `<h2>Confirm your email</h2>
+    <p><a href="${action_link}">Click here</a> or enter this code: ${email_otp}</p>`,
+    })
+  } catch (error) {
+    console.error("Failed to send login email:", error)
+  }
 }
