fbe243bcd796d336a41d226b019620e7dfd97e32
pato <patoliveshere@gmail.com>
2024-06-27 18:38:41 -0700

    Simplify READ pub_keys permissions, doesn't need partners names

--- src/supabase/policies/select-pub-keys.sql
+++ src/supabase/policies/select-pub-keys.sql
@@ -3,10 +3,8 @@ on "public"."pub_keys"
 to public
 using (
   user_id IN (
-      SELECT p.inviter
-      FROM get_partnerships_with_names() p;
-    UNION
-      SELECT p.invitee
-      FROM get_partnerships_with_names() p;
+    SELECT inviter FROM partnerships
+  UNION
+    SELECT invitee FROM partnerships
   )
 );
