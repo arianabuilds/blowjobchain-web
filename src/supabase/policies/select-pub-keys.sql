b58b090d243b9ff50d722a17674263d15c4ec651
pato <patoliveshere@gmail.com>
2024-06-27 17:25:57 -0700

    Fix pub_key partner read permissions

--- src/supabase/policies/select-pub-keys.sql
+++ src/supabase/policies/select-pub-keys.sql
@@ -2,15 +2,11 @@ alter policy "Partners can read each other's pub keys"
 on "public"."pub_keys"
 to public
 using (
-    (user_id = auth.uid()) -- own user id
-      OR 
-    (user_id IN ( -- partners' user ids
-        SELECT partnerships.inviter
-            FROM partnerships
-            WHERE (partnerships.invitee = auth.uid())
-        UNION
-        SELECT partnerships.invitee
-            FROM partnerships
-            WHERE (partnerships.inviter = auth.uid())
-    ))
+  user_id IN (
+      SELECT p.inviter
+      FROM get_partnerships_with_names() p;
+    UNION
+      SELECT p.invitee
+      FROM get_partnerships_with_names() p;
+  )
 );
