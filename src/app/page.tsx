c81e81b7d237e1be74098f5d03c08424f019b999
pato <patoliveshere@gmail.com>
2025-03-08 00:10:25 -0600

    Confirm <HomePage /> doesn't use params or searchParams, which became async

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,2 +1,1 @@
-export { /* @next-codemod-error `HomePage` export is re-exported. Check if this component uses `params` or `searchParams`*/
-HomePage as default } from "./HomePage"
+export { HomePage as default } from "./HomePage"
