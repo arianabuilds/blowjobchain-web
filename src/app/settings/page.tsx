81ee0fcebe868f213e4b4a40c1dda0186e6a5184
pato <patoliveshere@gmail.com>
2025-03-08 02:42:07 -0600

    next15 upgrade: Confirm <SettingsPage/> doesn't use params

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,2 +1,1 @@
-export { /* @next-codemod-error `SettingsPage` export is re-exported. Check if this component uses `params` or `searchParams`*/
-SettingsPage as default } from "./SettingsPage"
+export { SettingsPage as default } from "./SettingsPage"
