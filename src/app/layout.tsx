f8701e03be1401e54e26fe95ce314822ae4a8e75
pato <patoliveshere@gmail.com>
2025-03-10 01:40:04 -0500

    Remove buggy 'Inter' font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata, Viewport } from "next"
-import { Inter } from "next/font/google"
 import "./globals.css"
-
-const inter = Inter({ subsets: ["latin"] })
 
 export const viewport: Viewport = { maximumScale: 1, userScalable: false }
 export const metadata: Metadata = { title: "BlowjobChain", description: "" }
@@ -16,7 +13,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         style={{ WebkitUserSelect: "none" }}
-        className={`from-[#42002E] to-[#4e031f] bg-gradient-to-b h-[100cqh] flex flex-col items-center overflow-hidden text-white/70 select-none cursor-default ${inter.className}`}
+        className="from-[#42002E] to-[#4e031f] bg-gradient-to-b h-[100cqh] flex flex-col items-center overflow-hidden text-white/70 select-none cursor-default"
       >
         {children}
       </body>
