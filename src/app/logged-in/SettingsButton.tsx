928c78b72a0089e3e7a4abcabf7e9f1d8960764b
Ariana I <git@arianaivan.com>
2025-03-30 21:32:33 -0700

    SettingButton: Fix right alignment

--- src/app/logged-in/SettingsButton.tsx
+++ src/app/logged-in/SettingsButton.tsx
@@ -3,7 +3,7 @@ import { IoSettingsOutline } from "react-icons/io5"
 
 export const SettingsButton = () => {
   return (
-    <div className="hover:bg-white/10 rounded-full absolute right-5 top-[17px] cursor-pointer z-10 p-2 opacity-80 active:bg-white/15">
+    <div className="hover:bg-white/10 rounded-full absolute right-[18px] top-[17px] cursor-pointer z-10 p-2 opacity-80 active:bg-white/15">
       <Link href="settings">
         <IoSettingsOutline size={22} />
       </Link>
