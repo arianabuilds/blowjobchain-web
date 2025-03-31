fe311afd14d1bc46cda1f702877511c8de86d733
Ariana I <git@arianaivan.com>
2025-03-30 21:05:26 -0700

    Use outlined settings icon

--- src/app/logged-in/SettingsButton.tsx
+++ src/app/logged-in/SettingsButton.tsx
@@ -1,11 +1,11 @@
 import Link from "next/link"
-import { IoMdSettings } from "react-icons/io"
+import { IoSettingsOutline } from "react-icons/io5"
 
 export const SettingsButton = () => {
   return (
-    <div className="hover:bg-white/10 rounded-full absolute right-5 top-[18px] cursor-pointer z-10 p-2 opacity-80 active:bg-white/15">
+    <div className="hover:bg-white/10 rounded-full absolute right-5 top-[17px] cursor-pointer z-10 p-2 opacity-80 active:bg-white/15">
       <Link href="settings">
-        <IoMdSettings size={22} />
+        <IoSettingsOutline size={22} />
       </Link>
     </div>
   )
