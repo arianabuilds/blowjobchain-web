1973c72ba90dc40061f3cf0dc172b206a1d0b736
pato <patoliveshere@gmail.com>
2025-03-30 23:40:40 -0700

    Remove all awaits from /settings initial load

--- src/app/logged-in/PrePartner.tsx
+++ src/app/logged-in/PrePartner.tsx
@@ -1,11 +1,11 @@
-import { HelloName } from "./HelloName"
+import { HelloNameClient } from "../settings/name/HelloNameClient"
 import { InvitePartnerLink } from "./InvitePartnerLink"
 import { NoRecordsYet } from "./NoRecordsYet"
 
 export const PrePartner = ({ name }: { name: string }) => {
   return (
     <div className="text-center flex-1 mt-20 w-full px-4 max-w-[22rem] mx-auto">
-      <HelloName name={name} />
+      <HelloNameClient name={name} />
 
       <NoRecordsYet />
 
