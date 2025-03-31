5e7b71e211408249853cfc5d1bb6c4bf5690f639
Ariana I <git@arianaivan.com>
2025-03-30 21:21:03 -0700

    Thick, sparkling logo

--- src/app/Logo.tsx
+++ src/app/Logo.tsx
@@ -1,9 +1,12 @@
 export const Logo = ({ big }: { big?: boolean }) => (
   <p
     className={`
-    font-medium tracking-tight bg-gradient-to-r from-pink-200 via-white to-purple-200 bg-clip-text text-transparent drop-shadow-[0_2px_8px_rgba(236,72,153,0.3)]
-    ${big ? "text-4xl pt-20" : "text-2xl pt-5"}
-  `}
+      font-bold tracking-tight
+      bg-gradient-to-b from-white via-white/90 to-white/20
+      bg-clip-text text-transparent
+      [text-shadow:_2px_2px_8px_rgba(255,255,255,0.15)]
+      ${big ? "text-4xl mt-20" : "text-2xl mt-5"}
+    `}
   >
     BlowjobChain
   </p>
