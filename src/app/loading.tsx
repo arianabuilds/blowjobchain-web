7e9bd0326a6c09bb5d5182cd9534cef65022e437
pato <patoliveshere@gmail.com>
2026-01-06 20:38:23 -0800

    loading.tsx: y-Center the spinner

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@ import { CgSpinner } from "react-icons/cg"
 
 export default function Loading() {
   return (
-    <div className="mt-20 mb-20 animate-spin opacity-50">
+    <div className="my-auto animate-spin opacity-50">
       <CgSpinner size={64} />
     </div>
   )
