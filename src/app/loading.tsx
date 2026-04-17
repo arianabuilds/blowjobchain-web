fe02992515cacb62eca2963743f0c1e625a5cdc0
pato <patoliveshere@gmail.com>
2026-04-16 22:10:05 -0700

    More sleek loading icon

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,9 @@
-import { CgSpinner } from "react-icons/cg"
+import { ImSpinner9 } from "react-icons/im"
 
 export default function Loading() {
   return (
     <div className="my-auto animate-spin opacity-50">
-      <CgSpinner size={64} />
+      <ImSpinner9 size={40} />
     </div>
   )
 }
